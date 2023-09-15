e4e3b8fafc363a9e61316bb30da619a44ce9f805
polatksk <polatksk@hotmail.com>
2023-09-15 11:25:19 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryServices
+
+    {
+
+
+        ICategoryDal _categorydal;
+
+      
+        public CategoryManager(ICategoryDal categorydal)
+        {
+            _categorydal = categorydal;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _categorydal.Insert(category);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categorydal.List();
+    }
+
+
+        //public void CategoryAddBl(Category p)
+        //{
+        //    if (p.CategoryName=="" || p.CategoryStatus==false || p.CategoryName.Length<=2)
+        //    {
+        //        //hata mesajı
+        //    }
+
+        //    else
+        //    {
+        //        _categorydal.Insert(p);
+        //    }
+        //}
+    }
+
+}
---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,62 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using FluentValidation.Results;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidationResult = FluentValidation.Results.ValidationResult;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCatgoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);    
+        }
+        [HttpGet]
+        public ActionResult AddCategory() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult AddCategory(Category p)
+        {
+            //   cm.CategoryAddBL(p);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCatgoryList");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+
+                }
+
+
+            }
+            return View();
+        }
+    }
+}
